28f14e4da2bb1031b95b6c6a1ddebcf7b9a0eb76
Winston Hearn <hello@wnstn.com>
2024-09-19 10:40:42 -0400

    using hfo package

--- themes/bloggin/assets/js/tracing.js
+++ themes/bloggin/assets/js/tracing.js
@@ -1,59 +1,25 @@
 import * as params from '@params';
+import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 
-// This is the tracing factory, as well as a helper to batch spans (reducing network traffic)
-import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
-// a set of auto instrumentations for the web
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { ZoneContextManager } from "@opentelemetry/context-zone";
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { Resource }  from '@opentelemetry/resources';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-// instrumentation we want
-import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
-import { WebVitalsInstrumentation } from './helpers/core-web-vitals-instrumentation';
-import { DocumentLoadSpans, ResourceFetchSpans } from './helpers/document-load-custom-spans';
-import { getOrSetSessionID } from './helpers/session-data';
+const configDefaults = {
+  ignoreNetworkEvents: true,
+  // propagateTraceHeaderCorsUrls: [
+  // /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
+  // ]
+}
 
-const SESSION_ID = getOrSetSessionID();
-
-const exporter = new OTLPTraceExporter({
-  url: "https://api.honeycomb.io/v1/traces",
-  headers: {
-    "x-honeycomb-team": params.env.hny,
-  },
+const sdk = new HoneycombWebSDK({
+  debug: true, // Set to false for production environment.
+  apiKey: params.env.hny, // Replace with your Honeycomb Ingest API Key.
+  serviceName: params.env.service, // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
+  instrumentations: [getWebAutoInstrumentations({
+    // Loads custom configuration for xml-http-request instrumentation.
+    '@opentelemetry/instrumentation-xml-http-request': configDefaults,
+    '@opentelemetry/instrumentation-fetch': configDefaults,
+    '@opentelemetry/instrumentation-document-load': configDefaults,
+  })],
 });
+sdk.start();
 
-const provider = new WebTracerProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: params.env.service,
-    'session_id': SESSION_ID,
-    'session.window.width': window.innerWidth,
-    'session.window.height': window.innerHeight,
-    'session.device.width': window.outerWidth,
-    'session.device.height': window.outerHeight,
-    'session.user_agent': navigator.userAgent,
-  })
-});
-
-const processor = new BatchSpanProcessor(exporter);
-
-provider.addSpanProcessor(processor);
-provider.register({
-  contextManager: new ZoneContextManager(),
-});
-
-registerInstrumentations({
-  instrumentations: [
-    new WebVitalsInstrumentation(),
-    new DocumentLoadInstrumentation({
-      applyCustomAttributesOnSpan: {
-          documentLoad: DocumentLoadSpans,
-          resourceFetch: ResourceFetchSpans,
-      }
-    }),
-    // new UserInteractionInstrumentation(),
-    // new ResourceTiming(),
-    // new ErrorCatching(),
-  ],
-});
-
+// Application instantiation code
