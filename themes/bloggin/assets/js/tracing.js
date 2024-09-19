bd333cd824fd7106f6cee8c0a4dc1c9c4dd1a9d7
Winston Hearn <hello@wnstn.com>
2024-09-19 10:47:31 -0400

    update config, remove debug

--- themes/bloggin/assets/js/tracing.js
+++ themes/bloggin/assets/js/tracing.js
@@ -9,10 +9,11 @@ const configDefaults = {
   // ]
 }
 
+console.log(params);
+
 const sdk = new HoneycombWebSDK({
-  debug: true, // Set to false for production environment.
-  apiKey: params.env.hny, // Replace with your Honeycomb Ingest API Key.
-  serviceName: params.env.service, // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
+  apiKey: params.env.hny, // netlify admin
+  serviceName: params.env.service, // netlify admin
   instrumentations: [getWebAutoInstrumentations({
     // Loads custom configuration for xml-http-request instrumentation.
     '@opentelemetry/instrumentation-xml-http-request': configDefaults,
@@ -21,5 +22,3 @@ const sdk = new HoneycombWebSDK({
   })],
 });
 sdk.start();
-
-// Application instantiation code
